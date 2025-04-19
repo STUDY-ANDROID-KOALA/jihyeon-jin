b1bd629a61c222d60fc9357b6fa2f6833080ece9
serioushyeon <zz64446949@gmail.com>
2025-04-20 04:52:23 +0900

    [2주차] 프로그래머스_Lv3_여행경로.kt 주석 추가

--- week2/프로그래머스_Lv3_여행경로.kt
+++ week2/프로그래머스_Lv3_여행경로.kt
@@ -1,5 +1,3 @@
-package com.koala.study.serious.week1
-
 fun main() {
     val sv = Solution2()
     val tickets = arrayOf(
@@ -12,15 +10,26 @@ fun main() {
     sv.solution(tickets = tickets)
 
 }
-
+/*
+모든 경로 탐색 DFS + 백트래킹 사용
+티켓 수 + 1이 경로의 수 -> 이때 라우트 리스트에 추가
+얕은 복사 주의 (routes.add(route)) -> 얕은 복사
+(routes.add(ArrayList(route))) -> 깊은 복사
+혹은 val routes = mutableListOf<List<String>>()
+routes.add(route.toList()) 하면 깊은 복사
+sortBy({it.joinToString(seperator="")})
+sortWith(compareBy{ it.joinToStrong("") })
+둘 다 단어 연결함 구분자 명시 안하면 ,가 포함됨
+val routes = PriorityQueue<List<String>>(compareBy{ it.joinToString("") })
+이렇게 선언하면 sort 없이 poll한게 정답임
+ */
 class Solution2 {
     fun solution(tickets: Array<Array<String>>): Array<String> {
         val ticketCount = tickets.count()
         val visited = BooleanArray(ticketCount) { false }
 
-        val route = arrayListOf<String>()
+        val route = mutableList<String>()
         val routes = ArrayList<ArrayList<String>>()
-
         route.add("ICN")
 
         fun dfs(destination: String, route: ArrayList<String>) {
